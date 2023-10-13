144aacc35335398be360371859725679f18120bc
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2023-10-13 16:48:32 +0100

    done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/analytics.png'
-import IMG2 from '../../assets/fitkeep.png'
-import IMG3 from '../../assets/userdashboard.png'
-import IMG4 from '../../assets/weather.png'
-import IMG5 from '../../assets/quizapp.png'
-import IMG6 from '../../assets/IMG6.jpg'
-import IMG7 from '../../assets/anony.jpg'
+import IMG1 from '../../assets/analytics.png';
+import IMG2 from '../../assets/fitkeep.png';
+import IMG3 from '../../assets/userdashboard.png';
+import IMG4 from '../../assets/weather.png';
+import IMG5 from '../../assets/quizapp.png';
+import IMG6 from '../../assets/IMG6.jpg';
+import IMG7 from '../../assets/anony.jpg';
+import IMG8 from '../../assets/eCommerce .jpg';
 const Portfolio = () => {
      const handleLink = (url)=>{
           window.location.href = url;
@@ -58,7 +59,7 @@ const Portfolio = () => {
             <div className="portfolio__item-image">
              <img src={IMG5} alt=''/>
             </div>
-            <h3>Quiz App</h3
+            <h3>Quiz App</h3>
             <a href='https//github.com' className='btn' target='_blank'>Github</a>
              <a href='https://fidel-wole.github.io/Bio-101-questions' style={{marginLeft:15}} onClick={()=>handleLink('https://fidel-wole.github.io/Bio-101-questions')}className='btn' >View</a>
        </article>
@@ -69,6 +70,14 @@ const Portfolio = () => {
             <h3>Quiz Hub built with react js</h3>
             <a href='https//github.com' className='btn' target='_blank'>Github</a>
              <a href='https://fidel-wole.github.io/React-Js-Quiz-App' style={{marginLeft:15}} onClick={()=>handleLink('https://fidel-wole.github.io/React-Js-Quiz-App')}className='btn' >View</a>
+       </article>
+       <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+             <img src={IMG8} alt=''/>
+            </div>
+            <h3>E-commerce built with node js</h3>
+            <a href='https//github.com/fidel-wole' className='btn' target='_blank'>Github</a>
+
        </article>
     </div>
   </section>
